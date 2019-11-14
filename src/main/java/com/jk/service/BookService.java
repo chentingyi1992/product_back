0cba60b1f1d667b312e389702f1e267a1962c5c6
yangdd109224 <2048114149@qq.com>
2019-11-14 16:19:35 +0800

    更新

--- src/main/java/com/jk/service/BookService.java
+++ src/main/java/com/jk/service/BookService.java
@@ -1,8 +1,9 @@
 package com.jk.service;
 
 
-import com.jk.model.UserBean;
+import com.jk.model.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -15,4 +16,14 @@ public interface BookService {
 
 
     UserBean findUserByName(String username);
+
+    HashMap<String, Object> findBookList(Integer page, Integer rows, HousBean housBean);
+
+    void addUser(HousBean housBean);
+
+    List<AreaBean> findDeptList();
+
+    List<TownBean> queryTown(Integer id);
+
+    List<CommBean> queryComm(Integer id);
 }
